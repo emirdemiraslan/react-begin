eef39bb30f60edcdea658d833767932040b7f312
Emir Demiraslan <emir.demiraslan@gmail.com>
2017-11-15 00:35:15 +0300

    Routes

--- src/app/index.js
+++ src/app/index.js
@@ -1,65 +1,28 @@
 import React from "react";
 import {render} from "react-dom";
-import {Header} from "./components/Header";
+import {Router, Route} from "react-router";
+import createBrowserHistory from 'history/createBrowserHistory';
+const newHistory = createBrowserHistory();
+
+import {Root} from "./components/Root";
 import {Home} from "./components/Home";
+import {User} from "./components/User";
+
+
 
 class App extends React.Component {
 
-  constructor() {
-    super();
-    this.state = {
-      homeLink: "Home",
-      homeMounted:true
-    }
-  }
-
-  onGreet(name) {
-    alert("Hello! " + name);
-  }
-
-  onChangeLinkName(newName) {
-    this.setState({
-      homeLink:newName
-    });
-  }
-
-  onChangeHomeMounted() {
-    this.setState({
-      homeMounted:!this.state.homeMounted
-    });
-  }
-
   render() {
-    let homeComp ="";
-    if(this.state.homeMounted) {
-      homeComp= <Home
-        name={"Emir"}
-        initialAge={34}
-        greet={this.onGreet}
-        changeLink={this.onChangeLinkName.bind(this)}
-        initialLink={this.state.homeLink}
-      />
-    }
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col-md-10 col-md-offset-1">
-            <Header homeLink={this.state.homeLink}/>
-          </div>
-        </div>
+     <Router history={newHistory}>
+         <div>
+             <Route exact path="/" component={Home} />
+             <Route path="/" component={Root} />
+             <Route path="/user" component={User} />
+             <Route path="/home" component={Home}/>
+         </div>
 
-        <div className="row">
-          <div className="col-md-10 col-md-offset-1">
-            {homeComp}
-          </div>
-        </div>
-
-        <div className="row">
-          <div className="col-md-10 col-md-offset-1">
-            <button onClick={this.onChangeHomeMounted.bind(this)} className="btn btn-primary">(Un)Mount Home Component</button>
-          </div>
-        </div>
-      </div>
+     </Router>
     );
   }
 
